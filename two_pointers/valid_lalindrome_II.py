f424dab4507cc3944a8c7f05689ef195e44d45a7
Bianca Torres <btorres@blueskyelearn.com>
2024-02-28 09:44:16 -0500

    wip

--- two_pointers/valid_lalindrome_II.py
+++ two_pointers/valid_lalindrome_II.py
@@ -3,22 +3,26 @@ def is_palindrome(sentence):
   right = len(sentence)-1
   skip=0
 
-  while left != right:
+  while left < right:
     if sentence[left] == sentence[right]:
         left+=1
         right-=1
-    if sentence[left] != sentence[right]:     
-       right-=1
-       skip
-
-    if skip > 1:
-       return False
-
-    left+=1
-    right-=1   
-      
-
+        l = sentence[left]
+        r = sentence[right]
+    else:
+       if sentence[left] != sentence[right] and skip < 1:     
+            left+=1
+            last_left=left
+            if sentence[left] != sentence[right]:
+                left-=1
+                right-=1
+                skip+=1
+                l = sentence[left]
+                r = sentence[right]
+       else:
+         return False        
+    
   return True
 
 
-print(is_palindrome("madame"))
+print(is_palindrome("abcdedadedecba"))
